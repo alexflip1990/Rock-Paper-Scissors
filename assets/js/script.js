7259b3f6247f34b4befdc43c0b62800a45963b17
alexflip1990 <>
2023-05-26 16:47:46 +0000

    Add constants for dom elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hey there');
+/**
+ * Create constants for dom elements 
+ * to be used later
+ */
+const player = document.getElementById('player');
+const computer = document.getElementById('computer');
+const playerScoreSpan = document.getElementById('player-score');
+const computerScoreSpan = document.getElementById('comp-score');
+const results = document.querySelector('#results');
+const rock = document.getElementsByClassName('rock');
+const paper = document.getElementsByClassName('paper');
+const scissors = document.getElementsByClassName('scissors');
+const restart = document.getElementsByClassName('restart');
