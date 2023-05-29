c4d0085fe70c52076536899364f77a2dfd857fb9
alexflip1990 <>
2023-05-29 17:29:21 +0000

    Removed unused variable 'result'

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ const player = document.getElementById('player');
 const computer = document.getElementById('computer');
 const playerScoreSpan = document.getElementById('player-score');
 const computerScoreSpan = document.getElementById('comp-score');
-const results = document.querySelector('#results');
 const rockDiv = document.getElementById('rock');
 const paperDiv = document.getElementById('paper');
 const scissorsDiv = document.getElementById('scissors');
